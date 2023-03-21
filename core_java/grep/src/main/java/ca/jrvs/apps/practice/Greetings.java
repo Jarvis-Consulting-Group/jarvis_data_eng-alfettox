d085dc311a754fa4b1d22fb0ec63f1b723ac2d74
Giovanni De Franceschi <38984306+alfettox@users.noreply.github.com>
2023-03-21 14:43:39 -0400

    Develop (#10)
    * solve ignore problem
    
    * create core_java project structure
    
    * create core_java project structure
    
    * create core_java project structure
    
    * JavaGrep interface
    
    * grep completed but missed outputh path
    
    * fixed javagrep and javagrepimp but still problems with output
    
    * corrected general readme

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/Greetings.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/Greetings.java
@@ -1,26 +1,26 @@
-package ca.jrvs.apps.practice;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Scanner;
-
-public class Greetings {
-
-    public static void main(String[] args) {
-        String name = "";
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        name = in.nextLine();
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        int hours = cal.get(Calendar.HOUR_OF_DAY);
-
-        if(hours < 11)
-            System.out.println(   "Good morning, " + name);
-        else if (hours <15)
-            System.out.println("Good afternoon, " + name);
-        else
-            System.out.println("Good evening, " + name);
-
-    }
-}
+//package ca.jrvs.apps.practice;
+//
+//import java.util.Calendar;
+//import java.util.Date;
+//import java.util.Scanner;
+//
+//public class Greetings {
+//
+//    public static void main(String[] args) {
+//        String name = "";
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter your name: ");
+//        name = in.nextLine();
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTime(new Date());
+//        int hours = cal.get(Calendar.HOUR_OF_DAY);
+//
+//        if(hours < 11)
+//            System.out.println(   "Good morning, " + name);
+//        else if (hours <15)
+//            System.out.println("Good afternoon, " + name);
+//        else
+//            System.out.println("Good evening, " + name);
+//
+//    }
+//}
